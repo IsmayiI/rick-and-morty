b6fb7e2e7027e02fc28c4c8e5ddfb858efcafd6f
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-05-24 17:22:51 +0400

    add characters route

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,16 @@
 import { Logo } from "@components/icons"
+import { ROUTES } from "@utils/constans"
+import Link from "next/link"
 
 export const Header = () => {
+
    return (
       <header className="bg-gray-300">
          <div className="container flex justify-between items-center py-4 text-slate-700">
             <Logo />
             <nav>
                <ul>
-                  <li>Characters</li>
+                  <li><Link href={ROUTES.CHARACTERS}>Characters</Link></li>
                </ul>
             </nav>
          </div>
